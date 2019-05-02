28e450a6864577b78a37b671fcfdfd2d74242b3f
Kev <pwntastickevin@gmail.com>
2019-05-02 00:17:49 -0600

    assets folder, added icon and menu looks good for mobile.

--- src/screens/App/screens/PublicLayout/Header/Header.js
+++ src/screens/App/screens/PublicLayout/Header/Header.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import companyLogo from '../../shared/assets/copperCreekConcepts.gif'
 
-let Wrapper = styled.section` display: flex; justify-content: space-between; `
-let companyLogo = styled.img``
-let navMenu = styled.menu` div { height: 2px; display: flex; justify-content: space-between; flex-direction: column; }`
-let navbarWrapper = styled.section` &:media(min-wdith:1024px) {display: none} `
-let navbarItem = styled.div``
+let Wrapper = styled.section` 
+	display: flex; 
+	justify-content: space-between; 
+	padding: 10px 15px;
+
+	img {
+		width: 95px;
+    	max-height: 30px;
+	}
+`
+let CompanyLogo = styled.div`
+`
+let NavMenu = styled.div`
+	text-align: center;
+ div { 
+	margin: 3px 0; 
+	background: gray; 
+	height: 3px; 
+	width: 25px; 
+	display: flex; 
+	justify-content: space-between; 
+	flex-direction: column; 
+}
+span {
+	font-size: 9px;
+	color: gray;
+	text-align: center;
+}
+`
+let NavbarWrapper = styled.section` display: none; @media(min-width: 1024px) { display: none } `
+let NavbarItem = styled.div``
 
 export default class Header extends Component {
   render() {
 	return (
 	  <div>
 		<Wrapper>
-			<companyLogo>loogo</companyLogo>
-			<navMenu>
+			<img src={companyLogo} alt="logoimg"/>
+			<NavMenu>
 				<div></div>
 				<div></div>
 				<div></div>
-			</navMenu>
-			<navbarWrapper>
-				<navbarItem></navbarItem>
-			</navbarWrapper>
+				<span>Menu</span>
+			</NavMenu>
+			<NavbarWrapper class='navwrap'>
+				<NavbarItem></NavbarItem>
+			</NavbarWrapper>
 		</Wrapper>
 	  </div>
 	)
--- src/screens/App/screens/PublicLayout/PublicLayout.jsx
+++ src/screens/App/screens/PublicLayout/PublicLayout.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Header from './Header/Header';
-import App from '../../App'
 
 export default class PublicLayouts extends Component {
   render() {
